7c2c3678ec8389db0382f8e8c6083580b2f60ae6
Edward <edwardthupane80@gmail.com>
2023-12-21 20:17:15 +0200

    Touch ups

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const popUp = document.getElementById('popUp');
 const exit = document.getElementById('exit');
 const searchBtn  = document.getElementById('searchBtn');
 const weatherDetails = document.getElementById('weatherDetails');
+const city = document.getElementById('city');
+const searchCity = document.getElementById('searchCity');
+const prev = document.getElementById('prev');
 const dataSet = "https://api.openweathermap.org/data/2.5/weather?lat=-26.0871908&lon=28.0124675&appid=d94ca0efe777cebdeea9d2b80f9f391b";
 
 fetch(dataSet)
@@ -21,4 +24,12 @@ exit.addEventListener('click',ex=>{
 searchBtn.addEventListener('click',btn=>{
     btn.preventDefault();
     weatherDetails.style.visibility = "visible";
+    city.innerHTML = searchCity.value;
+    searchCity.value = "";
+})
+
+prev.addEventListener('click',pre=>{
+    pre.preventDefault();
+    weatherDetails.style.visibility = "hidden";
+
 })
