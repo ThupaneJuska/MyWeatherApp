93b163b879550772ab97a4b16e2f5829fd2bf03e
Edward <edwardthupane80@gmail.com>
2023-12-22 22:54:23 +0200

    latest code

--- script.js
+++ script.js
@@ -6,7 +6,29 @@ const weatherDetails = document.getElementById('weatherDetails');
 const city = document.getElementById('city');
 const searchCity = document.getElementById('searchCity');
 const prev = document.getElementById('prev');
-const dataSet = "https://api.openweathermap.org/data/2.5/weather?lat=-26.0871908&lon=28.0124675&appid=d94ca0efe777cebdeea9d2b80f9f391b";
+const dataSet = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=pretoria&appid=d94ca0efe777cebdeea9d2b80f9f391b";
+const apiKey="d94ca0efe777cebdeea9d2b80f9f391b";
+
+fetch(dataSet)
+    .then(promise => promise.json())
+    .then(response =>{
+        document.querySelector("city-name").innerHTML = data.name;
+    })
+
+// async function checkWeather(){
+//     const response = await fetch(dataSet);
+//     var data = await response.json();
+
+//     console.log(data);
+    
+//     document.querySelector("degrees").innerHTML = Math.floor(data.main.temp);
+//     document.querySelector("city-name").innerHTML = data.name;
+   
+    
+    
+// }
+
+// checkWeather()
 
 fetch(dataSet)
     .then(promise =>promise.json())
@@ -24,8 +46,8 @@ exit.addEventListener('click',ex=>{
 searchBtn.addEventListener('click',btn=>{
     btn.preventDefault();
     weatherDetails.style.visibility = "visible";
-    city.innerHTML = searchCity.value;
-    searchCity.value = "";
+    // city.innerHTML = searchCity.value;
+    // searchCity.value = "";
 })
 
 prev.addEventListener('click',pre=>{
