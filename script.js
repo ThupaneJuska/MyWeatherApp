5d1a6851d972ded524994002cada30c105a2a3d2
Edward <edwardthupane80@gmail.com>
2023-12-23 22:01:43 +0200

    Latest javaScript

--- script.js
+++ script.js
@@ -4,15 +4,32 @@ const exit = document.getElementById('exit');
 const searchBtn  = document.getElementById('searchBtn');
 const weatherDetails = document.getElementById('weatherDetails');
 const city = document.getElementById('city');
-const searchCity = document.getElementById('searchCity');
+const searchCity = document.getElementById('searchCity').value;
+const imgIcon = document.getElementById('imgIcon');
 const prev = document.getElementById('prev');
-const dataSet = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=pretoria&appid=d94ca0efe777cebdeea9d2b80f9f391b";
 const apiKey="d94ca0efe777cebdeea9d2b80f9f391b";
+const cite = prompt("Enter city name")
+const dataSet = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`;
+// const iconId = data.weather[0].icon;
+// const iconUrl = `http://openweathermap.org/img/w/${iconId}.png`;
 
-fetch(dataSet)
+// document.getElementById('weather-icon').src = iconUrl;
+
+
+
+
+fetch(dataSet+cite+`&appid=${apiKey}`)
     .then(promise => promise.json())
     .then(response =>{
-        document.querySelector("city-name").innerHTML = data.name;
+        // city.innerHTML = response.name;
+        document.querySelector(".city-name").innerHTML = response.name;
+        document.querySelector(".degrees").innerHTML = response.main.temp +"°C";
+        const iconId = response.weather[0].icon;
+        const iconUrl = `http://openweathermap.org/img/w/${iconId}.png`;
+        document.getElementById('imgIconW').src = iconUrl;
+        document.querySelector('.humid').innerHTML = response.main.humidity + "%";
+        document.querySelector('.wind-speed').innerHTML = response.wind.speed + "km/h";
+
     })
 
 // async function checkWeather(){
@@ -30,7 +47,7 @@ fetch(dataSet)
 
 // checkWeather()
 
-fetch(dataSet)
+fetch(dataSet+cite+`&appid=${apiKey}`)
     .then(promise =>promise.json())
     .then(results=>console.log(results ))
 
@@ -46,8 +63,8 @@ exit.addEventListener('click',ex=>{
 searchBtn.addEventListener('click',btn=>{
     btn.preventDefault();
     weatherDetails.style.visibility = "visible";
-    // city.innerHTML = searchCity.value;
-    // searchCity.value = "";
+     city.innerHTML = searchCity.value;
+    
 })
 
 prev.addEventListener('click',pre=>{
